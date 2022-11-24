c289f39cb7d4c057ec7e1d2b3d345084d775e598
Dongeun Lee <sixgen@gmail.com>
2022-11-25 01:26:17 +0900

    fix demo

---
+++ demo/remote-server.js
@@ -0,0 +1,3 @@
+import { RemoteServer ,WS_PORT } from 'remote-signal'
+
+const remoteServer = new RemoteServer({ port: WS_PORT } )
--- demo_web/remote_image/client.js
+++
@@ -1,7 +0,0 @@
-import { Remocon } from 'remocon'
-
-const remocon = new Remocon('ws://localhost:3002')
-
-remocon.sub("#screen",(e,data)=>{
-  console.log( e, data )
-})
